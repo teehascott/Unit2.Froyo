d75c3bd3bafe122ce89a5482a3f7d9b8bbd17600
Teeha Scott <teehascott@gmail.com>
2024-11-15 13:31:41 -0500

    idk not working

---
+++ javacript.js
@@ -0,0 +1,22 @@
+// prompt for froyo flavors
+let flavorsList = prompt("vanilla,chocolate,coffee,cake batter,cookie dough,strawberry,pineapple,orange");
+
+// splits at the comma and makes an array
+let flavorsArray = flavorsList.split(',');
+
+// makes empty object to count flavors? (not working?)
+let flavorNum = {};
+
+// keep the numbers of each flavor ordered? (not working?) also trim for whitespace
+for (let i = 0; i < flavorsArray.length; i++) { 
+    let flavor = flavorsArray[i].trim();  
+    if (flavorNum[flavor]) { 
+        flavorNum[flavor]++; 
+    } 
+    else {
+         flavorNum[flavor] = 1; 
+        }
+}
+
+// makes a table of the flavor count
+console.table(flavorNum);
